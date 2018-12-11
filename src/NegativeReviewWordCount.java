79b35a227581ec7f1fd483826fe3f147ff81aa43
Walt <wpniederer@cpp.edu>
2018-12-10 20:25:28 -0800

    fixed issues with parsing bad data

--- src/NegativeReviewWordCount.java
+++ src/NegativeReviewWordCount.java
@@ -17,33 +17,25 @@ public class NegativeReviewWordCount {
 
     public static class BusinessReviewMapper extends Mapper<Object, Text, Text, IntWritable> {
 
-//        private Text business_id = new Text();
     private final static IntWritable one = new IntWritable(1);
-
-        private Text businessID = new Text();
-//        private Text word = new Text();
-//        private Text starRating = new Text();
-//        private Text reviewText = new Text();
 
         public void map(Object key, Text value, Context context ) throws IOException, InterruptedException {
             String[] singleReview = value.toString().split("\\|");
 
-            String word = "";
-            // 3th index is the review
-            StringTokenizer reviewItr = new StringTokenizer(singleReview[3]);
-
             if(singleReview.length >= 9){
+                // 3th index is the review
+                StringTokenizer reviewItr = new StringTokenizer(singleReview[3]);
+                String word = "";
+
                 // 8th index is the star rating
                 if (Integer.parseInt(singleReview[8]) < 3) {
                     while(reviewItr.hasMoreTokens()) {
-//                        businessID.set(line[2]);
                         word = reviewItr.nextToken();
+
                         // 4th index is the businessID
-                        context.write(new Text(singleReview[4] + '&' + word), one);
+                        context.write(new Text(singleReview[4] + ' ' + word), one);
                     }
 
-//                    reviewText.set(line[5]);
-//                    context.write(businessID, reviewText);
                 }
             }
 
@@ -77,10 +69,7 @@ public class NegativeReviewWordCount {
         job.setReducerClass(BusinessReviewReducer.class);
 
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
-
-        job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
 
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
